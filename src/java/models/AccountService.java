9f62a7c56ac3a406d0700992f993dacc36ba6d3f
allen <allen@10.0.0.130>
2023-02-12 23:05:59 -0700

    Created both jsp and account service and user

--- src/java/models/AccountService.java
+++ src/java/models/AccountService.java
@@ -11,4 +11,17 @@ package models;
  */
 public class AccountService {
     
+    public AccountService() {
+        
+    }
+    
+    public User login(String username, String password) {
+        if ((username.equals("abe") && password.equals("password")) || (username.equals("barb") && password.equals("password"))) {
+            
+            return new User(username, null);
+        }
+        else {
+            return null;
+        }
+    }
 }
--- src/java/models/User.java
+++ src/java/models/User.java
@@ -5,10 +5,42 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author allen
  */
-public class User {
+public class User implements Serializable {
     
+    String username;
+    
+    String password;
+    
+    public User() {
+        this.username = null;
+        
+        this.password = null;
+    }
+    
+    public User(String username, String password) {
+    
+        this.username = username;
+        
+        this.password = password;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
