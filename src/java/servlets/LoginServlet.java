8acaa6480edd77e743942ca3a32f7ca7b7f24a89
allen <allen@10.0.0.130>
2023-02-12 23:31:55 -0700

    created both servlets

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -6,81 +6,85 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.AccountService;
+import models.User;
 
 /**
  *
  * @author allen
  */
 public class LoginServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet LoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet LoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        String logout = request.getParameter("logout");
+         
+        // if logout exists, user wants to log out
+        if (logout != null) {
+            request.getSession().invalidate();
+            request.setAttribute("message", "Successfully logged out.");
+            
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
+
+        // if no user, and don't want to log out
+        if (logout == null && request.getSession().getAttribute("user") == null) {
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+        }
+
+        // if user already exists
+        if (request.getSession().getAttribute("user") != null) {
+            response.sendRedirect("home");
+        }
+        
+       
+
+        
+        
+        
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        request.setAttribute("username", username);
+        request.setAttribute("password", password);
+        
+        // check that data was not entered
+        if (username.equals("") || username == null || password.equals("") || password == null) {
+
+            request.setAttribute("message", "Username or password invalid.");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+
+        } else { // if data was entered
+
+            AccountService check = new AccountService();
+            User user = check.login(username, password);
+
+            // if not valid user
+            if (user == null) {
+                request.setAttribute("message", "Not a valid login");
+                getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+
+                // if is valid user
+            } else {
+                request.getSession().setAttribute("user", user);
+                response.sendRedirect("home");
+            }
+
+        }
+            
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+        
+        
+        
 }
